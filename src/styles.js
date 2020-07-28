2de5c13c0bc5a1fdbc7c866aa5be8c4f52663d05
Manoj Nataraja <manojnataraja@Manojs-MacBook-Air.local>
2020-07-28 11:45:40 +0530

    initial commit

---
+++ src/styles.js
@@ -0,0 +1,85 @@
+export const styles = {
+    header: {
+      backgroundColor: 'black',
+      height: '50px',
+      pad: '500px'
+    },
+    text: {
+      color: 'white',
+      pad: '10px',
+      size: "small"
+    },
+    img: {
+      margin: '10px',
+      pad: '30px',
+      height: '30px',
+      width: '30px',
+    },
+    search: {
+      height: '30px',
+      weight: '1000px',
+    },
+    div: {
+      marginBottom: '20px',
+    },
+    repoName: {
+      color: '#0099ff'
+    },
+    langdate: {
+      marginLeft: '10px',
+      color: '#606060',
+      fontSize: '15px'
+    },
+    rightdiv: {
+      width: '80%'
+    },
+    mrightdiv: {
+      width: '100%',
+      marginLeft: '10px',
+      marginRight: '30px'
+    },
+    leftdiv: {
+      margin: '25px',
+      width: '20%'
+    },
+    mleftdiv: {
+      width: '100%',
+      margin: '20px'
+    },
+    rootdiv: {
+      width: '100%',
+      display: 'flex'
+    },
+    mrootdiv: {
+      width: '100%',
+    },
+    personaImage: {
+      height: '300px',
+      borderRadius: '50%'
+    },
+    mpersonaImage: {
+      height: '60px',
+      borderRadius: '50%'
+    },
+    mmargin: {
+      width: '80%'
+    },
+    headingFont: {
+      marginTop: '10px',
+      marginLeft: '10px',
+      marginBottom: '0px',
+      fontSize: '30px'
+    },
+    loginfont: {
+      fontSize: '20px',
+      color: '#808080',
+      fontWeight: 'normal',
+      marginBottom: '10px'
+    },
+    followDiv:{
+      display: 'flex',
+      width:'90%',
+      alignText: 'center',
+      marginBottom: '20px'
+    }
+  }
